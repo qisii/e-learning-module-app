ddb6ab29ea71993729e5000d15e13d4f71c3ad52
quisiecaporte <quisiecaporte@gmail.com>
2025-12-06 13:46:44 +0800

    fixing frontend errors - users/admin

--- resources/views/users/profile/show.blade.php
+++ resources/views/users/profile/show.blade.php
@@ -7,24 +7,18 @@
 @section('account-active', 'bg-[#0F2250] text-blue-300')
 
 @section('content')
-    <div class="w-full h-full overflow-auto m-0 p-0">
-
-        {{-- Background section --}}
-        <div class="w-full h-[40%] bg-cover bg-bottom relative"
-             style="background-image: url('{{ asset('assets/images/profile-header-simple1.png') }}');">
-        </div>
-
-        {{-- Form section --}}
-        <div class="w-full px-4 lg:px-10 md:px-10 -mt-[15%] lg:-mt-[25%] md:-mt-[30%]">
-            <div class="bg-white w-full mx-auto shadow-lg rounded-lg p-0 pt-6 lg:p-6 md:p-6">
+<div class="h-auto">
+    <div class="w-full h-[400px] bg-cover bg-bottom relative"
+        style="background-image: url('{{ asset('assets/images/profile-header-simple1.png') }}');">
+        <div class="w-full px-4 lg:px-10 md:px-10 absolute translate-y-[10%] lg:translate-y-[35%] md:translate-y-[50%]">
+            <div class="bg-white w-full mx-auto shadow-lg rounded-lg p-0 pt-6 mb-5 lg:p-6 md:p-6 mb-6 lg:mb-6">
                 
                 <livewire:profile-form :user="$user" />
                 
             </div>
         </div>
-
     </div>
-    
+</div>
 @endsection
 
 
