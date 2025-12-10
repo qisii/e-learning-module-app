5c6b2a2fc97a10f09bfc811b0beecddf4f933c14
quisiecaporte <quisiecaporte@gmail.com>
2025-12-10 08:00:15 +0800

    fixing persistent storage - yaml

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -65,52 +65,95 @@ class GradeController extends Controller
         return redirect()->back()->with('success', 'Old quiz attempts deleted successfully.');
     }
 
-   public function searchPretest(Request $request)
+    public function searchPretest(Request $request)
     {
-        $search = $request->search;
+        $search = $request->input('search');
+        $date = $request->input('date'); // NEW
 
         $pretests = QuizAttempt::with('quiz.folder.project', 'quiz.questions')
             ->where('user_id', Auth::id())
             ->whereHas('quiz.folder', function ($query) {
                 $query->where('folder_type_id', 1); // Pretest
             })
-            ->where(function ($query) use ($search) {
-                $query->whereHas('quiz.folder.project', function ($q) use ($search) {
-                    $q->where('title', 'like', "%$search%");
-                })
-                ->orWhere('score', 'like', "%$search%")
-                ->orWhere('attempt_number', 'like', "%$search%");
+            ->when($search, function ($query) use ($search) {
+                $query->where(function ($q) use ($search) {
+
+                    // Project title
+                    $q->whereHas('quiz.folder.project', function ($p) use ($search) {
+                        $p->where('title', 'like', "%$search%");
+                    })
+
+                    // Score
+                    ->orWhere('score', 'like', "%$search%")
+
+                    // Attempt number
+                    ->orWhere('attempt_number', 'like', "%$search%");
+
+                    // Time spent in mm:ss format
+                    if (preg_match('/^(\d+):(\d+)$/', $search, $matches)) {
+                        $seconds = ($matches[1] * 60) + $matches[2];
+                        $q->orWhere('time_spent', $seconds);
+                    }
+                });
+            })
+            ->when($date, function ($query) use ($date) {  // NEW DATE FILTER
+                $query->whereDate('created_at', $date);
             })
             ->latest()
             ->paginate(8)
-            ->appends(['search' => $search]);
+            ->appends([
+                'search' => $search,
+                'date'   => $date,
+            ]);
 
         return view('users.grades.index', compact('pretests'));
     }
 
-
     public function searchPosttest(Request $request)
     {
-        $search = $request->search;
+        $search = $request->input('search');
+        $date = $request->input('date'); // NEW
 
         $posttests = QuizAttempt::with('quiz.folder.project', 'quiz.questions')
             ->where('user_id', Auth::id())
             ->whereHas('quiz.folder', function ($query) {
                 $query->where('folder_type_id', 3); // Post-test
             })
-            ->where(function ($query) use ($search) {
-                $query->whereHas('quiz.folder.project', function ($q) use ($search) {
-                    $q->where('title', 'like', "%$search%");
-                })
-                ->orWhere('score', 'like', "%$search%")
-                ->orWhere('attempt_number', 'like', "%$search%");
+            ->when($search, function ($query) use ($search) {
+                $query->where(function ($q) use ($search) {
+
+                    // Project title
+                    $q->whereHas('quiz.folder.project', function ($p) use ($search) {
+                        $p->where('title', 'like', "%$search%");
+                    })
+
+                    // Score
+                    ->orWhere('score', 'like', "%$search%")
+
+                    // Attempt number
+                    ->orWhere('attempt_number', 'like', "%$search%");
+
+                    // Time spent in mm:ss format
+                    if (preg_match('/^(\d+):(\d+)$/', $search, $matches)) {
+                        $seconds = ($matches[1] * 60) + $matches[2];
+                        $q->orWhere('time_spent', $seconds);
+                    }
+                });
+            })
+            ->when($date, function ($query) use ($date) { // NEW DATE FILTER
+                $query->whereDate('created_at', $date);
             })
             ->latest()
             ->paginate(8)
-            ->appends(['search' => $search]);
+            ->appends([
+                'search' => $search,
+                'date'   => $date,
+            ]);
 
         return view('users.grades.post-test', compact('posttests'));
     }
+
+
 
 
     // ---------- ADMIN SIDE ----------
@@ -147,6 +190,7 @@ class GradeController extends Controller
     public function searchPretestAdmin(Request $request)
     {
         $search = $request->input('search');
+        $date = $request->input('date'); // NEW
 
         $pretests = QuizAttempt::with('user', 'quiz.folder.project', 'quiz.questions')
             ->whereHas('quiz.folder', function ($query) {
@@ -155,22 +199,44 @@ class GradeController extends Controller
             ->whereHas('quiz.folder.project', function ($query) {
                 $query->where('user_id', Auth::id()); // Projects by admin
             })
-            ->where(function ($query) use ($search) {
-                $query->whereHas('user', function ($q) use ($search) {
-                    $q->where('first_name', 'like', "%$search%")
-                    ->orWhere('last_name', 'like', "%$search%")
-                    ->orWhere('grade_level', 'like', "%$search%")
-                    ->orWhere('section', 'like', "%$search%");
-                })
-                ->orWhereHas('quiz.folder.project', function ($q) use ($search) {
-                    $q->where('title', 'like', "%$search%");
-                })
-                ->orWhere('score', 'like', "%$search%")
-                ->orWhere('attempt_number', 'like', "%$search%");
+            ->when($search, function ($query) use ($search) {
+                $query->where(function ($q) use ($search) {
+
+                    // User filters
+                    $q->whereHas('user', function ($u) use ($search) {
+                        $u->where('first_name', 'like', "%$search%")
+                        ->orWhere('last_name', 'like', "%$search%")
+                        ->orWhere('grade_level', 'like', "%$search%")
+                        ->orWhere('section', 'like', "%$search%");
+                    })
+
+                    // Project title
+                    ->orWhereHas('quiz.folder.project', function ($p) use ($search) {
+                        $p->where('title', 'like', "%$search%");
+                    })
+
+                    // Score
+                    ->orWhere('score', 'like', "%$search%")
+
+                    // Attempt number
+                    ->orWhere('attempt_number', 'like', "%$search%");
+
+                    // Time spent (mm:ss format)
+                    if (preg_match('/^(\d+):(\d+)$/', $search, $matches)) {
+                        $seconds = ($matches[1] * 60) + $matches[2];
+                        $q->orWhere('time_spent', $seconds);
+                    }
+                });
+            })
+            ->when($date, function ($query) use ($date) { // NEW DATE FILTER
+                $query->whereDate('created_at', $date);
             })
             ->latest()
             ->paginate(8)
-            ->appends(['search' => $search]); // Keep search query in pagination
+            ->appends([
+                'search' => $search,
+                'date'   => $date, // Keep date in pagination
+            ]);
 
         return view('admin.grades.index')->with('pretests', $pretests);
     }
@@ -178,6 +244,7 @@ class GradeController extends Controller
     public function searchPostTestAdmin(Request $request)
     {
         $search = $request->input('search');
+        $date = $request->input('date'); // NEW
 
         $posttests = QuizAttempt::with('user', 'quiz.folder.project', 'quiz.questions')
             ->whereHas('quiz.folder', function ($query) {
@@ -186,22 +253,44 @@ class GradeController extends Controller
             ->whereHas('quiz.folder.project', function ($query) {
                 $query->where('user_id', Auth::id());
             })
-            ->where(function ($query) use ($search) {
-                $query->whereHas('user', function ($q) use ($search) {
-                    $q->where('first_name', 'like', "%$search%")
-                    ->orWhere('last_name', 'like', "%$search%")
-                    ->orWhere('grade_level', 'like', "%$search%")
-                    ->orWhere('section', 'like', "%$search%");
-                })
-                ->orWhereHas('quiz.folder.project', function ($q) use ($search) {
-                    $q->where('title', 'like', "%$search%");
-                })
-                ->orWhere('score', 'like', "%$search%")
-                ->orWhere('attempt_number', 'like', "%$search%");
+            ->when($search, function ($query) use ($search) {
+                $query->where(function ($q) use ($search) {
+
+                    // User filters
+                    $q->whereHas('user', function ($u) use ($search) {
+                        $u->where('first_name', 'like', "%$search%")
+                        ->orWhere('last_name', 'like', "%$search%")
+                        ->orWhere('grade_level', 'like', "%$search%")
+                        ->orWhere('section', 'like', "%$search%");
+                    })
+
+                    // Project title
+                    ->orWhereHas('quiz.folder.project', function ($p) use ($search) {
+                        $p->where('title', 'like', "%$search%");
+                    })
+
+                    // Score
+                    ->orWhere('score', 'like', "%$search%")
+
+                    // Attempt number
+                    ->orWhere('attempt_number', 'like', "%$search%");
+
+                    // Time spent (mm:ss format)
+                    if (preg_match('/^(\d+):(\d+)$/', $search, $matches)) {
+                        $seconds = ($matches[1] * 60) + $matches[2];
+                        $q->orWhere('time_spent', $seconds);
+                    }
+                });
+            })
+            ->when($date, function ($query) use ($date) { // NEW DATE FILTER
+                $query->whereDate('created_at', $date);
             })
             ->latest()
             ->paginate(8)
-            ->appends(['search' => $search]);
+            ->appends([
+                'search' => $search,
+                'date'   => $date, // Keep date in pagination
+            ]);
 
         return view('admin.grades.post-test')->with('posttests', $posttests);
     }
@@ -239,6 +328,7 @@ class GradeController extends Controller
     public function searchModuleAdmin(Request $request)
     {
         $search = $request->input('search');
+        $date = $request->input('date'); // NEW
 
         // Map level names to IDs
         $levelMap = [
@@ -254,35 +344,55 @@ class GradeController extends Controller
             ->whereHas('handout.folder.project', function ($q) {
                 $q->where('user_id', Auth::id()); // Projects created by this admin
             })
-            ->where(function ($query) use ($search, $searchLevelId) {
-                // User filters
-                $query->whereHas('user', function ($q) use ($search) {
-                    $q->where('first_name', 'like', "%$search%")
-                    ->orWhere('last_name', 'like', "%$search%")
-                    ->orWhere('username', 'like', "%$search%")
-                    ->orWhere('grade_level', 'like', "%$search%")
-                    ->orWhere('section', 'like', "%$search%");
-                })
-                // Project title filter
-                ->orWhereHas('handout.folder.project', function ($q) use ($search) {
-                    $q->where('title', 'like', "%$search%");
-                })
-                // Level filter (only if searching by level name)
-                ->orWhere(function ($q) use ($searchLevelId) {
-                    if ($searchLevelId) {
-                        $q->whereHas('handout', function ($h) use ($searchLevelId) {
-                            $h->where('level_id', $searchLevelId);
-                        });
+            ->when($search, function ($query) use ($search, $searchLevelId) {
+                $query->where(function ($q) use ($search, $searchLevelId) {
+
+                    // User filters
+                    $q->whereHas('user', function ($u) use ($search) {
+                        $u->where('first_name', 'like', "%$search%")
+                        ->orWhere('last_name', 'like', "%$search%")
+                        ->orWhere('username', 'like', "%$search%")
+                        ->orWhere('grade_level', 'like', "%$search%")
+                        ->orWhere('section', 'like', "%$search%");
+                    })
+
+                    // Project title
+                    ->orWhereHas('handout.folder.project', function ($p) use ($search) {
+                        $p->where('title', 'like', "%$search%");
+                    })
+
+                    // Level filter
+                    ->orWhere(function ($lvl) use ($searchLevelId) {
+                        if ($searchLevelId) {
+                            $lvl->whereHas('handout', function ($h) use ($searchLevelId) {
+                                $h->where('level_id', $searchLevelId);
+                            });
+                        }
+                    })
+
+                    // Attempt number
+                    ->orWhere('attempt_number', 'like', "%$search%");
+
+                    // Time spent in mm:ss format
+                    if (preg_match('/^(\d+):(\d+)$/', $search, $matches)) {
+                        $seconds = ($matches[1] * 60) + $matches[2];
+                        $q->orWhere('time_spent', $seconds);
                     }
-                })
-                // Attempt number or time spent
-                ->orWhere('attempt_number', 'like', "%$search%")
-                ->orWhere('time_spent', 'like', "%$search%");
+
+                });
+            })
+            // Date filter
+            ->when($date, function ($query) use ($date) {
+                $query->whereDate('created_at', $date);
             })
             ->latest()
             ->paginate(8)
-            ->appends(['search' => $search]);
+            ->appends([
+                'search' => $search,
+                'date'   => $date,
+            ]);
 
         return view('admin.grades.module')->with('modules', $modules);
     }
+
 }
--- resources/views/admin/grades/post-test.blade.php
+++ resources/views/admin/grades/post-test.blade.php
@@ -14,43 +14,63 @@
     {{-- Tabs Section --}}
     <div class="flex items-center gap-2 mb-6 bg-gray-100 rounded-xl">
         <a href="{{ route('admin.grades.pretest') }}"
-            class="px-6 py-4 text-[12px] rounded-xl text-[#6B7280] hover:text-[#374151] transition">
+            class="px-6 py-4 text-[10px] lg:text-[12px] rounded-xl text-[#6B7280] hover:text-[#374151] transition">
             Pretest
         </a>
 
         <a href="{{ route('admin.grades.posttest') }}"
-            class="px-6 py-4 text-[12px] rounded-xl bg-[#E5E7EB] text-[#374151] font-semibold cursor-default">
+            class="px-6 py-4 text-[10px] lg:text-[12px] rounded-xl bg-[#E5E7EB] text-[#374151] font-semibold cursor-default">
             Post-test
         </a>
 
         <a href="{{ route('admin.grades.module') }}"
-            class="px-6 py-4 text-[12px] rounded-xl text-[#6B7280] hover:text-[#374151] transition">
+            class="px-6 py-4 text-[10px] lg:text-[12px] rounded-xl text-[#6B7280] hover:text-[#374151] transition">
             Module
         </a>
     </div>
 
     {{-- Post Test Content --}}
-    <div class="bg-white rounded-xl shadow-md p-6 text-[12px]">
+    <div class="bg-white rounded-xl shadow-md p-6 text-[12px] overflow-auto">
 
-        {{-- Search Input --}}
-        <div class="flex items-center space-x-2 mb-4">
-            <div class="relative w-[50%] lg:w-[30%]">
-                <form action="{{ route('admin.grades.posttest.search') }}" method="get">
+        {{-- Filter Input --}}
+        <div class="flex flex-col sm:flex-row w-full sm:items-center justify-end gap-3 lg:gap-4">
+            <form action="{{ route('admin.grades.posttest.search') }}" method="get" class="flex flex-col sm:flex-row w-full sm:items-center gap-3 lg:gap-4">
+
+                {{-- Search --}}
+                <div class="relative w-full sm:w-[50%] lg:w-[20%]">
                     <input 
                         id="search"
                         type="search" 
                         name="search"
                         value="{{ request('search') }}"
                         placeholder="Search here..." 
-                        class="border border-gray-300 rounded-lg pl-10 pr-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
+                        class="border border-gray-300 rounded-lg pl-10 pr-3 py-2 focus:outline-none 
+                            focus:ring-2 focus:ring-blue-500 w-full"
                     >
                     <i class="ri-search-line absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"></i>
-                </form>
-            </div>
+                </div>
+                {{-- Date --}}
+                <div class="w-full sm:w-[50%] lg:w-[20%]">
+                    <input 
+                        type="date" 
+                        name="date"
+                        value="{{ request('date') }}"
+                        class="border border-gray-300 rounded-lg px-3 py-2 focus:outline-none 
+                            focus:ring-2 focus:ring-blue-500 text-[12px] w-full"
+                    >
+                </div>
+
+                {{-- Submit Button (optional) --}}
+                <button type="submit"
+                    class="px-4 py-2 bg-blue-600 text-white rounded-lg text-[12px] hover:bg-blue-700 transition">
+                    Filter
+                </button>
+
+            </form>
         </div>
 
         {{-- Table --}}
-        <div class="overflow-x-auto">
+        <div class="overflow-x-auto mt-5">
             <table class="min-w-full border-collapse table-auto">
                 <thead class="bg-[#f0f4ff] text-gray-700 uppercase">
                     <tr>
